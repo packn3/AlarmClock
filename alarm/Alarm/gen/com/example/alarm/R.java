6d03b66f45faff74b4b798f7841866591771ceb6
SlimeSmile <nyaganyagame@gmail.com>
2013-12-26 17:13:25 +0900

    ボタンゲームの追加（未完）
    小野寺氏のボタンゲームを追加

--- alarm/Alarm/gen/com/example/alarm/R.java
+++ alarm/Alarm/gen/com/example/alarm/R.java
@@ -37,11 +37,14 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_alarm=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_button_game=0x7f030001;
+        public static final int activity_main=0x7f030002;
     }
     public static final class menu {
         public static final int alarm=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int button=0x7f080001;
+        public static final int button_game=0x7f080002;
+        public static final int main=0x7f080003;
     }
     public static final class raw {
         public static final int gyoza=0x7f040000;
@@ -52,6 +55,8 @@ public final class R {
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
         public static final int title_activity_alarm=0x7f060003;
+        public static final int title_activity_button=0x7f060004;
+        public static final int title_activity_button_game=0x7f060005;
     }
     public static final class style {
         /** 
